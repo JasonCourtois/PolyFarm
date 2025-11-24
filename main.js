aac9dae54a07c0fa5b2cd1d2c8a176e247335415
JasonCourtois <148589763+JasonCourtois@users.noreply.github.com>
2025-11-23 23:00:22 -0500

    next create random movement

--- main.js
+++ main.js
@@ -1,40 +1,49 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import Cow from './cow.js'
 
-var renderer, controls, scene, camera;
+// Scene variables
+let renderer, controls, scene, camera;
+// Array for all animals
+let animals = [];
+
+let worldSize = 200;
 
 window.onload = function () {
   // create scene
   scene = new THREE.Scene();
   
   // setup the camera
-  var fov = 75;
-  var ratio = window.innerWidth / window.innerHeight;
-  var zNear = 1;
-  var zFar = 10000;
+  let fov = 75;
+  let ratio = window.innerWidth / window.innerHeight;
+  let zNear = 1;
+  let zFar = 10000;
   camera = new THREE.PerspectiveCamera(fov, ratio, zNear, zFar);
-  camera.position.set(0, 0, 100);
+  camera.position.set(0, 150, 50);
 
   // create renderer and add canvas
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  //.. setup lights
-  var ambientLight = new THREE.AmbientLight();
+  // setup lights
+  let ambientLight = new THREE.AmbientLight();
   scene.add(ambientLight);
 
-  var light = new THREE.DirectionalLight(0xffffff, 5.0);
+  let light = new THREE.DirectionalLight(0xffffff, 5.0);
   light.position.set(10, 100, 10);
   scene.add(light);
 
-  // configure cube
-  var geometry = new THREE.BoxGeometry(20, 20, 20);
-  var material = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  var cube = new THREE.Mesh(geometry, material);
+  let planeGeometry = new THREE.PlaneGeometry(worldSize, worldSize);
+  let planeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, side: THREE.DoubleSide })
+  let plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  // Make the plane lie horizontal on the XZ ground plane
+  plane.rotateX(Math.PI / 2);
+  plane.position.y -= 10.1;
+  scene.add(plane);
 
-  // add it to the scene
-  scene.add(cube);
+  // Add cow ()
+  let cow = new Cow(scene, animals);
 
   // setup interaction
   controls = new OrbitControls(camera, renderer.domElement);
@@ -45,7 +54,10 @@ window.onload = function () {
 
 function animate() {
   requestAnimationFrame(animate);
-
+  animals.forEach(animal => {
+    animal.animate();
+    console.log(animal.cube.position);
+   });
   controls.update();
   renderer.render(scene, camera);
 }
